ceaeeba2801c18b09c1b633e7fedc4b5f606673d
HeleneB <helene.blanche@hotmail.fr>
2024-06-16 11:47:36 +0200

    Hello World

---
+++ public/view.php
@@ -0,0 +1,23 @@
+<!-- ⛔ DO NOT MODIFY THIS FILE ⛔-->
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Test Php</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1><?php echo $hello->talk(); ?></h1>
+    </div>
+</body>
+</html>
