951608ab736e1e9292f3a79b44f51cdb3790bd0b
HeleneB <helene.blanche@hotmail.fr>
2024-06-16 12:46:37 +0200

    add cowsay

--- public/index.php
+++ public/index.php
@@ -8,3 +8,20 @@ $hello = new Hello();
 
 // Do not modify code below
 require 'view.php';
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+$bessie = new Cow('Hello, Farm!');
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+// echo $bessie;
+
+// store the output in a variable
+//<pre> tag is used to define preformatted text
+// use the pre tag to preserve the whitespace and line breaks in the output
+$output = $bessie->say();
+echo '<pre class="cow-say">' . $output . '</pre>';
+
+
